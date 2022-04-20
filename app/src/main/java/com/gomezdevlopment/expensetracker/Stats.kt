23c4cc7bdd9328c92f2908a5b288cff659bf2cdb
Nathan.Gomez <gomezdevlopment@gmail.com>
2022-04-20 15:36:40 -0400

    Update stats activity to display horizontal bar chart

--- app/src/main/java/com/gomezdevlopment/expensetracker/Stats.kt
+++ app/src/main/java/com/gomezdevlopment/expensetracker/Stats.kt
@@ -4,10 +4,17 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.HorizontalBarChart
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.MPPointF
+import com.gomezdevlopment.expensetracker.database.UserEntry
+import com.gomezdevlopment.expensetracker.database.ViewModel
 import com.gomezdevlopment.expensetracker.databinding.StatsBinding
 
 class Stats: AppCompatActivity() {
@@ -27,12 +34,19 @@ class Stats: AppCompatActivity() {
         overviewValues.add(PieEntry(income, "Income"))
         setPieChart(overviewValues, binding.pieChart)
 
-        val detailedValues = ArrayList<PieEntry>()
-        detailedValues.add(PieEntry(100f, "food"))
-        detailedValues.add(PieEntry(200f, "gas"))
-        detailedValues.add(PieEntry(300f, "bills"))
-        detailedValues.add(PieEntry(1200f, "Income"))
-        setPieChart(detailedValues, binding.pieChartDetailed)
+        val detailedValues: ArrayList<BarEntry> = arrayListOf()
+        val userViewModel = ViewModelProvider(this)[ViewModel::class.java]
+        val xAxisLabels: ArrayList<String> = arrayListOf()
+
+        userViewModel.userEntries.observe(this) { entries ->
+            var pos = 0f
+            val sortedEntries = entries.sortedBy { it.value }
+            for(entry in sortedEntries) {
+                detailedValues.add(BarEntry(pos++, entry.value))
+                xAxisLabels.add(entry.title)
+            }
+            setBarChart(detailedValues, binding.barChartDetailed, xAxisLabels)
+        }
 
     }
 
@@ -49,6 +63,32 @@ class Stats: AppCompatActivity() {
         data.setValueTextSize(11f)
         data.setValueTextColor(Color.WHITE)
         chart.setHoleColor(ContextCompat.getColor(this, R.color.background))
+        chart.legend.isEnabled = false
+        chart.data = data
+        chart.description = null
+        chart.highlightValues(null)
+        chart.invalidate()
+        chart.animateXY(500, 500)
+    }
+
+    private fun setBarChart(values: ArrayList<BarEntry>, chart: HorizontalBarChart, labels: ArrayList<String>){
+
+        val barDataSet = BarDataSet(values, "Expenses and Income")
+
+        barDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
+
+        chart.axisLeft.isEnabled = false
+        chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        chart.xAxis.labelCount = labels.size
+        chart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        chart.xAxis.gridColor = ContextCompat.getColor(this, R.color.chartText)
+        chart.xAxis.textColor = ContextCompat.getColor(this, R.color.chartText)
+
+
+        val data = BarData(barDataSet)
+        data.setValueTextSize(11f)
+        data.setValueTextColor(ContextCompat.getColor(this, R.color.chartText))
+
         chart.legend.isEnabled = false
         chart.data = data
         chart.description = null
